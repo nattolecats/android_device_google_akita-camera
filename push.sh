78b96d158ff9de463cee723c41ced877896459d3
lahaina <nattolecats@gmail.com>
2024-05-24 15:55:22 +0900

    akita: Push instantly init script as well

--- push.sh
+++ push.sh
@@ -20,6 +20,7 @@ adb shell su 0 "rm -rf /vendor/apex_overlay"
 echo "* Pushing new blobs"
 adb push apex_overlay /vendor
 adb push rootdir/bin/camera_patch.sh /vendor/bin
+adb push rootdir/etc/camera_patch.rc /vendor/etc/init/hw
 
 echo "* Running bind-mount script"
 adb shell su 0 "sh /vendor/bin/camera_patch.sh"
